ded05d20e58af5d959b79fac7b28e28e6c6c880a
kerry <helinbo@qq.com>
2023-07-19 17:01:29 +0800

    up

--- lib/widgets/common_widgets.dart
+++ lib/widgets/common_widgets.dart
@@ -7,7 +7,8 @@ double e45 = 45;
 double e58 = 58;
 double e100 = 100;
 
-Color blueColor = const Color(0xFF5895F3);
+Color appBlueColor = const Color(0xFF5895F3);
+Color appBarBgColor = const Color(0xFFF7F8FC);
 
 Widget heightSpace(double height) {
   return SizedBox(
