78a04d2a59524c27d6799bb2e97e3318c396e9b0
kerry <helinbo@qq.com>
2023-07-19 11:16:52 +0800

    update

--- lib/basic_db.dart
+++ lib/basic_db.dart
@@ -3,3 +3,5 @@ export 'model/model.dart';
 export 'provider/db_provider.dart';
 export 'utils/utils.dart';
 export 'widgets/ink_well_button.dart';
+export 'widgets/common_widgets.dart';
+export 'utils/app_theme.dart';
---
+++ lib/widgets/common_widgets.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+double e10 = 10;
+double e18 = 18;
+double e28 = 28;
+double e45 = 45;
+double e58 = 58;
+double e100 = 100;
+
+Widget heightSpace(double height) {
+  return SizedBox(
+    height: height,
+  );
+}
+
+Widget widthSpace(double width) {
+  return SizedBox(
+    width: width,
+  );
+}
